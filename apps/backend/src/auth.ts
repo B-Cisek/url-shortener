9c531111276da8513b2ad7cafe7897e06129ce84
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2026-06-10 16:31:21 +0200

    feat(backend): database schema

---
+++ apps/backend/src/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { db } from './db/index.js'
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+  }),
+  advanced: {
+    database: {
+      generateId: 'uuid',
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+})
